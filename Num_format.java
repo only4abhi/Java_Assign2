46649ac7982e1c292ab753cc33efb173672e5756
only4abhi <abhishekchanda.mailbox@gmail.com>
2017-01-04 20:41:26 +0530

    Add files via upload

---
+++ Num_format.java
@@ -0,0 +1,20 @@
+
+public class Number {
+
+	public static void main(String args[]){
+		
+		int a = 5 ;
+		int b= a-- - --a;
+		int c=a--;
+		int d=a>>2;
+		int e=a&b;
+		
+	    System.out.println(b);
+	    System.out.println(c);
+	    System.out.println(d);
+	    System.out.println(e);
+	    
+
+
+	}
+}
